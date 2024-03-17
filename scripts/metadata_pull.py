1c4bf93a8f8faab74c959eade7a0f966a9007344
Jun Yi Yue <junyi.yue@outlook.com>
2024-03-17 14:58:19 +0000

    Add functionality to pull github metadata based on config file

--- scripts/metadata_pull.py
+++ scripts/metadata_pull.py
@@ -18,20 +18,29 @@ def obtain_collected_metadata_filename(repo_string):
     metadata_filename = repo_string_token_list[0] + "___" + repo_string_token_list[1]
     return metadata_filename
 
+# Returns None if filename doesn't exist (silent fail)
+def read_json(json_file_path): 
+    repo_data_obj = None
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as json_file:
+            repo_data_obj = json.load(json_file)
+    return repo_data_obj
+
 # returns empty dict if file doesn't exist
-def read_from_json(repo_string): # abstract this out into utilities
+# TODO: abstract this out into utilities
+def read_from_json(repo_string): 
     filename_repo_suffix = obtain_collected_metadata_filename(repo_string)
-    json_file_path = os.path.join(REPOS_INFO_PATH, f"github_metadata_{filename_repo_suffix}.json")
+    json_file_path = os.path.join(REPOS_INFO_PATH, f"github_metadata___{filename_repo_suffix}.json")
     repo_data_obj = {}
     if os.path.exists(json_file_path):
         with open(json_file_path, "r") as json_file:
             repo_data_obj = json.load(json_file)
     return repo_data_obj
 
-# TODO: save the dictionary into a list of dictionaries / json file 
-def save_to_json(repo_string, data_obj): # abstract this out into utilities
+# TODO: abstract this out into utilities
+def save_to_json(repo_string, data_obj): 
     filename_repo_suffix = obtain_collected_metadata_filename(repo_string)
-    json_file_path = os.path.join(REPOS_INFO_PATH, f"github_metadata_{filename_repo_suffix}.json")
+    json_file_path = os.path.join(REPOS_INFO_PATH, f"github_metadata__{filename_repo_suffix}.json")
     with open(json_file_path, 'w') as json_file:
         json.dump(data_obj, json_file, indent=4)
     logging.info(f"Processing completed! File hashes saved to {json_file_path}.")
@@ -48,29 +57,28 @@ def main():
     #     print(forkPage)
 
     argparser = ArgumentParser(description="Pull GitHub metadata of a given repo")
-    argparser.add_argument("repo_url", help="GitHub URL of the repo")
+    argparser.add_argument("repo_config_file", help="Config file containing list of target repo URLs")
 
     args = argparser.parse_args()
-    repo_string = get_repo_string_from_url(args.repo_url)
-
     g = Github()
-    print("rate_limit: ", g.get_rate_limit())
-    repo = g.get_repo(repo_string)
 
-    metadata_dict = read_from_json(repo_string)
-    print("initial metadata_dict: ", metadata_dict)
+    repo_list = read_json(args.repo_config_file)
 
-    timestamp = int(datetime.now().timestamp() * 1000)
-    metadata_at_timestamp_dict = {
-        "forks_count": repo.forks_count,
-        "stars_count": repo.stargazers_count
-    }
+    for target_repo_url in repo_list:
+        # parse through target repo url and GET the repo object 
+        target_repo_string = get_repo_string_from_url(target_repo_url)
+        target_repo = g.get_repo(target_repo_string)
 
-    metadata_dict[timestamp] = metadata_at_timestamp_dict
-    print("end metadata_dict: ", metadata_dict)
-
-    save_to_json(repo_string, metadata_dict)
-    
+        # Collect metadata of the target repo at specified timestamp
+        # then append it to the current json file for that target repo
+        metadata_dict = read_from_json(target_repo_string)
+        timestamp = int(datetime.now().timestamp() * 1000)
+        metadata_at_timestamp_dict = {
+            "forks_count": target_repo.forks_count,
+            "stars_count": target_repo.stargazers_count
+        }
+        metadata_dict[timestamp] = metadata_at_timestamp_dict
+        save_to_json(target_repo_string, metadata_dict)
 
     
 if __name__ == "__main__":
