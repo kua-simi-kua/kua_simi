6d543eab5e33a534af3bca4efa23dd712dbc4d46
Jun Yi Yue <junyi.yue@outlook.com>
2024-08-09 15:20:17 +0000

    split into reusable workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 token=$1
+file_path=$2
 
-cd ./scripts/ && python ./auth_repo_metadata_pull.py ../config/repos_list_long.json -t $1
+cd ./scripts/ && python ./auth_repo_metadata_pull.py $2 -t $1
