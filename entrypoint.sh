272a8be80303594af239fb5fc1eb33f3b19068b1
Jun Yi Yue <junyi.yue@outlook.com>
2024-08-10 04:52:01 +0000

    change order of args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-token=$1
-file_path=$2
+file_path=$1
+token=$2
 
-cd ./scripts/ && python ./auth_repo_metadata_pull.py $2 -t $1
+cd ./scripts/ && python ./auth_repo_metadata_pull.py $1 -t $2
