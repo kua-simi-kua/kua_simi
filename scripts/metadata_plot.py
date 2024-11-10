dee9285843a6bfdf8236e21897ac2bb2c089d57e
Jun Yi Yue <junyi.yue@outlook.com>
2024-11-10 14:30:08 +0000

    allow for combination of fields

--- scripts/metadata_plot.py
+++ scripts/metadata_plot.py
@@ -9,13 +9,18 @@ from utils import json_helper
 def main():
     argparser = ArgumentParser(description="Plot a graph of given metadata file")
     argparser.add_argument("metadata_file", help="Metadata file that contains data to be plotted")
-    argparser.add_argument("-f", "--field", help="Field to be plotted", required=True)
+    argparser.add_argument("-f", "--field", help="Field to be plotted", nargs = "*", required=True)
     args = argparser.parse_args()
 
     metadata_dict = json_helper.read_json(args.metadata_file)
 
     metadata_df = pd.DataFrame.from_dict(metadata_dict, orient='index')
-    plot_data_series = metadata_df.get(args.field)
+
+    field_list = args.field
+    plot_data_series = pd.Series()
+    for field in field_list:
+        field_series = metadata_df.get(field)
+        plot_data_series = plot_data_series.add(field_series, fill_value=0)
     
     date_x_axis = [datetime.fromtimestamp(float(epoch_timestamp)/1000).strftime("%d/%m/%Y") for epoch_timestamp in plot_data_series.index]
 
