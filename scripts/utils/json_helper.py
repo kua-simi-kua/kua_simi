9e2bf4b0633f06873b842309b57992f1f577e53c
Jun Yi Yue <junyi.yue@outlook.com>
2025-02-05 14:14:19 +0000

    create directory first

--- scripts/utils/json_helper.py
+++ scripts/utils/json_helper.py
@@ -1,6 +1,7 @@
 import json
 import os
 import logging
+import time
 
 # Returns None if filename doesn't exist (silent fail)
 def read_json(json_file_path): 
@@ -12,6 +13,11 @@ def read_json(json_file_path):
     return repo_data_obj
 
 def save_json(json_file_path, data_obj):
-    with open(json_file_path, 'w') as json_file:
+    dir_path = os.path.dirname(json_file_path)
+    print("finding ",dir_path)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path + "/")
+        print("creating ",dir_path)
+    with open(json_file_path, 'w+') as json_file:
         json.dump(data_obj, json_file, indent=4)
     logging.info(f"save_json() completed! {data_obj} saved to {json_file_path}.")
