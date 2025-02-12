599c0c54d4e3cf65570daf1c9c3e2c9cc48ebd0f
Jun Yi Yue <junyi.yue@outlook.com>
2025-02-12 15:10:36 +0000

    remove print statements

--- scripts/utils/json_helper.py
+++ scripts/utils/json_helper.py
@@ -14,10 +14,8 @@ def read_json(json_file_path):
 
 def save_json(json_file_path, data_obj):
     dir_path = os.path.dirname(json_file_path)
-    print("finding ",dir_path)
     if not os.path.exists(dir_path):
         os.makedirs(dir_path + "/")
-        print("creating ",dir_path)
     with open(json_file_path, 'w+') as json_file:
         json.dump(data_obj, json_file, indent=4)
     # logging.info(f"save_json() completed! {data_obj} saved to {json_file_path}.")
