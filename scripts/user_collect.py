350f72421fa43bc765acdcf9c2d2419885661267
Yue Jun Yi <yuejunyi@u.nus.edu>
2025-04-25 14:58:27 +0800

    collector script for user key info

--- scripts/user_collect.py
+++ scripts/user_collect.py
@@ -1,16 +1,85 @@
 from github import Github
 from github import Auth
 from argparse import ArgumentParser
-from datetime import datetime, date
-from utils import json_helper, constants
+from datetime import datetime
+from utils import json_helper, constants, datetime_helper
 import os
 
+
+class UserWrapper:
+    """
+    A simple wrapper to handle pulling user data from GitHub.
+    """
+    def __init__(self, user_obj):
+        self.user_obj = user_obj
+        try:
+            self.repos = user_obj.get_repos()
+        except Exception as e:
+            print(f"Error when get_repos() for {user_obj}")
+            self.repos = []
+    
+    def get_starred_list(self):
+        starred_list = []
+        for page in self.user_obj.get_starred():
+            try:
+                starred_list.append(page.full_name)
+            except Exception as e:
+                print(f"Error occured for {page}: \n {e}")
+                starred_list.append("Error")
+        return starred_list, len(starred_list)
+    
+    def get_subscriptions_list(self):
+        subscriptions_list = []
+        for page in self.user_obj.get_subscriptions():
+            try:
+                subscriptions_list.append(page.full_name)
+            except Exception as e:
+                print(f"Error occured for {page}: \n {e}")
+                subscriptions_list.append("Error")
+        return subscriptions_list, len(subscriptions_list)
+    
+    def get_forked_owned_list(self):
+        forked_list = []
+        owned_list = []
+        for page in self.repos:
+            try:
+                forked_list.append(page.full_name) if page.fork else owned_list.append(page.full_name)
+            except Exception as e:
+                print(f"Error occured for {page}: \n {e}")
+                forked_list.append("Error")
+                owned_list.append("Error")
+        return forked_list, len(forked_list), owned_list, len(owned_list)
+    
+    def get_dict_repr(self):
+        # forked_list, forked_count, owned_list, owned_count = self.get_forked_owned_list()
+        # subscriptions_list, subscriptions_count = self.get_subscriptions_list()
+        # starred_list, starred_count = self.get_starred_list()
+        user_key_info_dict = {
+            "location": self.user_obj.location,
+            "twitter_username": self.user_obj.twitter_username,
+            "created_at": datetime_helper.get_string_YYYYMMDD(self.user_obj.created_at),
+            "updated_at": datetime_helper.get_string_YYYYMMDD(self.user_obj.updated_at),
+            "followers": self.user_obj.followers,
+            "following": self.user_obj.following,
+            "public_gists_num": self.user_obj.public_gists,
+            "public_repos_num": self.user_obj.public_repos,
+            # "starred_list": starred_list,
+            # "starred_count": starred_count,
+            # "subscriptions_list": subscriptions_list,
+            # "subscriptions_count": subscriptions_count,
+            # "forked_list": forked_list,
+            # "forked_count": forked_count,
+            # "owned_list": owned_list,
+            # "owned_count": owned_count
+        }
+        return user_key_info_dict
 
 def main():
     argparser = ArgumentParser(description="Pull GitHub metadata of a given repo")
     argparser.add_argument("repo_string", help="repo_string to analyze.")
     argparser.add_argument("--token", "-t", help="Github token", nargs="?", default=None)
     argparser.add_argument("--date", "-d", help="Date of metadata to examine", default="latest")
+    argparser.add_argument("--interact_type", "-it", nargs="*", default=constants.MOST_RELEVANT_INTERACT_TYPES)
     args = argparser.parse_args()
 
     if args.token:
@@ -29,26 +98,50 @@ def main():
     else:
         metadata_full_filepath = metadata_repo_dir_path + args.repo_string + args.date + constants.JSON_SUFFIX
     
-    print(metadata_full_filepath)
     target_repo_dict = json_helper.read_json(metadata_full_filepath)
 
-    user_dict = {}
-    target_repo_stargazers = target_repo_dict.get("stargazers")
-    for stargazer in target_repo_stargazers:
-        print(str(g.get_rate_limit()))
-        user_obj = g.get_user(stargazer)
-        user_key_info = {
-            "location": user_obj.location,
-            "twitter_username": user_obj.twitter_username,
-            "created_at": user_obj.created_at,
-            "updated_at": user_obj.updated_at,
-            "followers": user_obj.followers,
-            "following": user_obj.following,
-            "public_gists": user_obj.public_gists,
-            "public_repos": user_obj.public_repos
-        }
-        user_dict[user_obj.login] = user_key_info        
+    user_list = []
+    for interact_type in args.interact_type:
+        user_list.extend(target_repo_dict[interact_type])
+    user_list = list(set(user_list))    
+    user_info_filepath = constants.USER_INFO_PATH + constants.USER_INFO_PREFIX + "1" + constants.JSON_SUFFIX
 
+    
+    for user in user_list:
+        rate_limit = g.get_rate_limit()
+        print(str(rate_limit))
+        if not rate_limit.core.remaining:
+            print(f"Rate limit is too low. Pls run again later (1 hour from {datetime_helper.get_string_YYYYMMDD_HHMMSS_UTC(datetime.now())})")
+
+        try:
+            entry_created_at = datetime_helper.get_string_YYYYMMDD_HHMMSS_UTC(datetime.now())
+            entry_updated_at = entry_created_at
+
+            user_dict = json_helper.read_json(user_info_filepath)
+            if not user_dict: 
+                user_dict = {}
+            elif user_dict.get(user):
+                old_entry_created_at = user_dict[user]["entry_created_at"]
+                old_entry_updated_at = user_dict[user]["entry_updated_at"]
+                if datetime_helper.is_within_timedelta_hours(entry_updated_at, old_entry_updated_at, 2):
+                    print(f"Not getting {user} since recently updated")
+                    continue
+                else:
+                    entry_created_at = old_entry_created_at
+            
+            print(f"Getting user {user}")
+            user_obj = g.get_user(user)
+            uw = UserWrapper(user_obj)
+            user_key_info_dict = uw.get_dict_repr()
+            user_key_info_dict["entry_created_at"] = entry_created_at
+            user_key_info_dict["entry_updated_at"] = entry_updated_at
+
+            user_dict[user] = user_key_info_dict
+            json_helper.save_json(user_info_filepath, user_dict)
+        except Exception as e:
+            print(f"Error occurred for {user}: \n {e}")
+            continue
+    
     
 
 
