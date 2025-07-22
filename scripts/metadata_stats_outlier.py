1a8673a5979a4d5be160819775624b8096e09636
Jun Yi Yue <junyi.yue@outlook.com>
2025-07-22 08:53:20 +0000

    skeleton for returning alerts based on 75

--- scripts/metadata_stats_outlier.py
+++ scripts/metadata_stats_outlier.py
@@ -2,8 +2,8 @@ from utils import json_helper, constants
 from argparse import ArgumentParser
 import os
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 import numpy as np
+from pprint import pprint
 
 def fill_in_missing_dates(counts_metadata_df):
     index_list = sorted(counts_metadata_df.index.to_list())
@@ -21,21 +21,34 @@ def fill_in_missing_dates(counts_metadata_df):
     counts_metadata_df.interpolate(method="linear", inplace=True)
     return counts_metadata_df
 
-# Function to perform statistical analysis of counts
-def stats_analyze(stats_dict, period):
+def stats_summary(stats_dict, period, target_date=None):
+    stats_df = pd.DataFrame(stats_dict).T
 
-    df = pd.DataFrame(stats_dict).T
-    df_period = df.tail(period)
-    # print(df_period)
+    if target_date:
+        stats_df = stats_df[stats_df.index <= target_date]
+    df_period = stats_df.tail(period)
 
     summary_df = df_period.describe()
-    print(summary_df)
+    summary_dict = summary_df.to_dict()
+    return df_period, summary_df, summary_dict
 
-    return summary_df
+def stats_summary_compare(summary_dict, stats_df):
+    target_row_dict = stats_df.tail(1).to_dict()
+    target_date = stats_df.index[-1]
+    print(target_date)
+    alert_row_dict = {}
+    for count_key in constants.COUNT_KEYS:
+        if target_row_dict[count_key][target_date] > summary_dict[count_key]['75%']:
+            alert_row_dict[count_key] = {
+                'target': target_row_dict[count_key][target_date],
+                '75': summary_dict[count_key]['75%']
+            }
+    return alert_row_dict
 
 def main():
     argparser = ArgumentParser(description="Dump stats on metadata")
     argparser.add_argument("metadata_dir", help="repos_info directory with metadata files")
+    argparser.add_argument("-td", "--target-date", help="target date to compare stats", nargs="?", default=None)
     args = argparser.parse_args()
 
     metadata_dir_list = []
@@ -44,9 +57,9 @@ def main():
         metadata_dir_list = os.listdir(all_metadata_dir_path)
     else:
         metadata_dir_list.append(args.metadata_dir)
-    
+
     for metadata_dir in metadata_dir_list:
-        print(f"Getting stats on {metadata_dir}")
+        print(f"Processing {metadata_dir}")
 
         stats_filename = metadata_dir + constants.STATS_SUFFIX + constants.JSON_SUFFIX
         stats_full_path = constants.REPOS_INFO_STATS_PATH + stats_filename
@@ -56,8 +69,37 @@ def main():
         cd_d_dict = stats_dict[constants.CD_D]
         ts_w_dict = stats_dict[constants.TS_W]
 
-        stats_analyze(cd_d_dict, 7)
-        # stats_analyze(cd_d_dict, 30)
+        cd_df_7, _, cd_7_summary = stats_summary(cd_d_dict, 7, args.target_date)
+        cd_df_30, _, cd_30_summary = stats_summary(cd_d_dict, 30, args.target_date)
+
+        if args.target_date: 
+            target_date = args.target_date 
+        else:
+            target_date = cd_df_7.index[-1]
+            
+        target_date_dict = dict()
+        check_list = [
+            ('cd_7', stats_summary_compare(cd_7_summary, cd_df_7)),
+            ('cd_30', stats_summary_compare(cd_30_summary, cd_df_30))
+        ]
+        for key, func in check_list:
+            if func: target_date_dict[key] = func
+
+        print(target_date_dict)
+
+
+
+
+
+        # TODO:
+        # {
+            # "ph4ntonn___Stowaway": {
+            #     "20250414": {
+            #         forks_count_ts_w_7: 89, 
+            #         forks_count_ts_w_7_75: 67
+            #     }
+            # }
+        # } 
 
         
 
