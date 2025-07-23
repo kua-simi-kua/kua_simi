a06849b0a1e46cfce1e748d9286bd58cbff147ad
Jun Yi Yue <junyi.yue@outlook.com>
2025-07-23 15:15:12 +0000

    modifying alerts log file

--- scripts/metadata_stats_outlier.py
+++ scripts/metadata_stats_outlier.py
@@ -35,7 +35,6 @@ def stats_summary(stats_dict, period, target_date=None):
 def stats_summary_compare(summary_dict, stats_df):
     target_row_dict = stats_df.tail(1).to_dict()
     target_date = stats_df.index[-1]
-    print(target_date)
     alert_row_dict = {}
     for count_key in constants.COUNT_KEYS:
         if target_row_dict[count_key][target_date] > summary_dict[count_key]['75%']:
@@ -49,7 +48,13 @@ def main():
     argparser = ArgumentParser(description="Dump stats on metadata")
     argparser.add_argument("metadata_dir", help="repos_info directory with metadata files")
     argparser.add_argument("-td", "--target-date", help="target date to compare stats", nargs="?", default=None)
+    argparser.add_argument("-l", "--log-file", help="log file to log alerts", nargs="?", default="")
     args = argparser.parse_args()
+
+    if len(args.log_file):
+        alerts_dict = json_helper.read_json(args.log_file)
+        if not alerts_dict:
+            alerts_dict = dict()
 
     metadata_dir_list = []
     if args.metadata_dir == "all":
@@ -66,26 +71,43 @@ def main():
         print(f"reading {stats_full_path}")
         stats_dict = json_helper.read_json(stats_full_path)
 
-        cd_d_dict = stats_dict[constants.CD]
-        ts_w_dict = stats_dict[constants.TSW]
+        cd_dict = stats_dict[constants.CD]
+        tsw_dict = stats_dict[constants.TSW]
 
-        cd_df_7, _, cd_7_summary = stats_summary(cd_d_dict, 7, args.target_date)
-        cd_df_30, _, cd_30_summary = stats_summary(cd_d_dict, 30, args.target_date)
+        cd_df_7, _, cd_7_summary = stats_summary(cd_dict, 7, args.target_date)
+        cd_df_30, _, cd_30_summary = stats_summary(cd_dict, 30, args.target_date)
+        tsw_df_7, _, tsw_7_summary = stats_summary(tsw_dict, 7, args.target_date)
+        tsw_df_30, _, tsw_30_summary = stats_summary(tsw_dict, 30, args.target_date)
 
-        if args.target_date: 
-            target_date = args.target_date 
+        if args.log_file:
+            if args.target_date: 
+                target_date = args.target_date 
+            else:
+                target_date = cd_df_7.index[-1]
+
+            target_date_dict = dict()
+            check_list = [
+                ('cd_7', stats_summary_compare(cd_7_summary, cd_df_7)),
+                ('cd_30', stats_summary_compare(cd_30_summary, cd_df_30)),
+                ('tsw_7', stats_summary_compare(tsw_7_summary, tsw_df_7)),
+                ('tsw_30', stats_summary_compare(tsw_30_summary, tsw_df_30))
+            ]
+            for key, func in check_list:
+                if func: target_date_dict[key] = func
+            
+            if len(target_date_dict):
+                if metadata_dir in alerts_dict:
+                    alerts_dict[metadata_dir].update({target_date: target_date_dict})
+                else:
+                    alerts_dict[metadata_dir] = {target_date: target_date_dict}
+            
+            json_helper.save_json(args.log_file, alerts_dict)
         else:
-            target_date = cd_df_7.index[-1]
+            continue
+        
 
-        target_date_dict = dict()
-        check_list = [
-            ('cd_7', stats_summary_compare(cd_7_summary, cd_df_7)),
-            ('cd_30', stats_summary_compare(cd_30_summary, cd_df_30))
-        ]
-        for key, func in check_list:
-            if func: target_date_dict[key] = func
 
-        print(target_date_dict)
+        
 
         
 
