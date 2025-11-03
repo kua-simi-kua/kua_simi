494c76030dc02362072361a95fec0b041fcd4624
Jun Yi Yue <junyi.yue@outlook.com>
2025-11-03 14:33:23 +0000

    add spacelaunchpad

---
+++ scripts/utils/SpaceLaunchPad.py
@@ -0,0 +1,22 @@
+import boto3
+
+class SpaceLaunchPad:
+    def __init__(self, key_id, key_secret, region="sgp1"):
+        self.key_id = key_id
+        self.key_secret = key_secret
+        self.bucket_name = "kua-simi-kua"
+        self.s3config = {
+            "region_name": region,
+            "endpoint_url": f"https://{region}.digitaloceanspaces.com",
+            "aws_access_key_id": self.key_id,
+            "aws_secret_access_key": self.key_secret }
+
+        self.s3resource = boto3.resource("s3", **self.s3config)
+        self.s3client = boto3.client("s3", **self.s3config)
+
+    def launch_to_space(self, key, file_path):
+        with open(file_path, 'rb') as data:
+            self.s3client.put_object(Bucket=self.bucket_name, Key=key, Body=data)
+
+    def get_from_space(self, key, file_path):
+        pass
