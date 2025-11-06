5aa6db03dfaecb529e9fe76b6e883e6b92f78050
Jun Yi Yue <junyi.yue@outlook.com>
2025-11-06 08:55:48 +0000

    get object

--- scripts/utils/SpaceLaunchPad.py
+++ scripts/utils/SpaceLaunchPad.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 class SpaceLaunchPad:
     def __init__(self, key_id, key_secret, region="sgp1"):
@@ -19,4 +20,7 @@ class SpaceLaunchPad:
             self.s3client.put_object(Bucket=self.bucket_name, Key=key, Body=data)
 
     def get_from_space(self, key, file_path):
-        pass
+        space_object = self.s3client.get_object(Bucket=self.bucket_name, Key=key)
+        space_load = json.loads(space_object["Body"].read().decode("utf-8"))
+        with open(file_path, 'w+') as json_file:
+            json.dump(space_load, json_file, separators=(",",":"))
